08d97be772c10fbd33f0bbb44c0e7f967125a526
benlecoz <go19111@bristol.ac.uk>
2022-10-27 17:42:29 +0100

    Added Graph Options to plot/savefig

--- generateAdj.jl
+++ generateAdj.jl
@@ -1,9 +1,8 @@
 using GraphPlot, Graphs
 
-function generateAdj(numNodes, graphType, graphParams)
+function generateAdj(numNodes, graphType, graphParams, graphOptions)
     if graphType == "complete"
         graph = complete_graph(numNodes)
-    
     end
 
     evenlySpaced = transpose(LinRange(0, 1, numNodes + 1))
@@ -11,13 +10,17 @@ function generateAdj(numNodes, graphType, graphParams)
     locs_x = vec(cos.(2*pi .* evenlySpaced))
     locs_y = vec(sin.(2*pi .* evenlySpaced))
 
-    graph_plot = gplot(graph, locs_x, locs_y)
-            
+    if graphOptions == "plot"
+        graph_plot = gplot(graph, locs_x, locs_y)
+    else
+        savefig(pwd() * "\\" * graphOptions * ".png")
+    end
+    
     return graph, graph_plot
    
 end
 
-graph, graph_plot = generateAdj(10, "complete", [])
+graph, graph_plot = generateAdj(10, "complete", [], "plot")
 
 graph_plot
 
