989989b7623e2741031e946cf7db9917d8dd0ecb
benlecoz <go19111@bristol.ac.uk>
2022-10-31 13:50:40 +0000

    Added more graph types and cleaned plotting code

--- generateAdj.jl
+++ generateAdj.jl
@@ -1,40 +1,60 @@
 using GraphPlot, Graphs
 
 function generateAdj(numNodes, graphType, graphParams, graphOptions)
+
+    plot = "normal"
+
     if graphType == "complete"
         graph = complete_graph(numNodes)
-
-        evenlySpaced = transpose(LinRange(0, 1, numNodes + 1))
-
-        locs_x = vec(cos.(2*pi .* evenlySpaced))
-        locs_y = vec(sin.(2*pi .* evenlySpaced))
+        plot = "circular plotting"
 
     elseif graphType == "WattsStrogatz"
         graph = watts_strogatz(numNodes, trunc(Int, graphParams[1]), graphParams[2])
+
     elseif graphType == "BarabasiAlbert"
         graph = barabasi_albert(numNodes, graphParams[1])
+    
     elseif graphType == "barbell"
         n = trunc(Int, floor(numNodes/2))
         graph = barbell_graph(n, n)
+    
     elseif graphType == "binaryTree"
         graph = binary_tree(numNodes)
+    
+    elseif graphType == "ErdosRenyi"
+        graph = erdos_renyi(numNodes, graphParams[1])
 
+    elseif graphType == "ring"
+        graph = cycle_graph(numNodes)
+        plot = "circular plotting"
+    
     end
 
     if isnothing(graphOptions) 
-    elseif graphOptions == "plot1"
-        graph_plot = gplot(graph, locs_x, locs_y)
-        graph_plot
-    elseif graphOptions == "plot2"
-        graph_plot = gplot(graph)
-        graph_plot
+
+    elseif graphOptions == "plot"
+
+        if plot == "circular plotting"
+            evenlySpaced = transpose(LinRange(0, 1, numNodes + 1))
+
+            locs_x = vec(cos.(2*pi .* evenlySpaced))
+            locs_y = vec(sin.(2*pi .* evenlySpaced))
+            graph_plot = gplot(graph, locs_x, locs_y)
+        
+        else
+            graph_plot = gplot(graph)
+        
+        end
+
     else
         savefig(pwd() * "\\" * graphOptions * ".png")
+
     end
     
     return graph, graph_plot
    
 end
 
-generateAdj(4, "binaryTree", [], "plot2")[2]
+generateAdj(12, "ErdosRenyi", [0.15], "plot")[2]
 
+Graphs.cycle_graph()
