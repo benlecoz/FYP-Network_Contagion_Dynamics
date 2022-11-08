f487441edf638cd8154b7641cc57b7c268d3145e
benlecoz <go19111@bristol.ac.uk>
2022-11-08 15:29:08 +0000

    Large scale code optimisation

--- sirGillespie.jl
+++ sirGillespie.jl
@@ -1,151 +1,160 @@
 include("./generateAdj.jl")
 using .AdjGenerator, Graphs, Distributions, SparseArrays
+using Profile, PProf, BenchmarkTools
 
-# generate Adjacency matrix
-numNodes = 50;
+function runSIRGillespie()
+    # generate Adjacency matrix
+    numNodes = 50;
 
-graphType = "complete";
-graphParams = [];
+    graphType = "complete";
+    graphParams = [];
 
-graph = AdjGenerator.generateAdj(numNodes, graphType, graphParams);
-Adj = adjacency_matrix(graph);
+    graph = AdjGenerator.generateAdj(numNodes, graphType, graphParams);
+    Adj = adjacency_matrix(graph);
 
-# define contagion dynamics parameters
-lambda = 1;
-gamma = 0.1;
+    # define contagion dynamics parameters
+    lambda = 1.;
+    gamma = 0.1;
 
-modelParams = [lambda, gamma];
+    modelParams = [lambda, gamma];
 
-# time parameters
-maxTime = 12;
-timeRes = 0.01;
+    # time parameters
+    maxTime = 12.;
+    timeRes = 0.01;
 
-maxTime = timeRes*ceil(maxTime/timeRes);
-t = Array(collect(Float64, 0:timeRes:maxTime)');
-numTimes = length(t);
+    maxTime = timeRes*ceil(maxTime/timeRes);
+    t = Array(collect(Float64, 0:timeRes:maxTime)');
+    numTimes = length(t);
 
-# initial conditions with one infected node
-initConds = zeros(numNodes, 1);
-initConds[1] = 1;
+    # initial conditions with one infected node
+    initConds = zeros(Int64, numNodes, 1);
+    initConds[1] = 1;
 
-# Gillespie model parameters
-numRuns = 10^3;
+    # Gillespie model parameters
+    numRuns = 10^4;
 
-storedStates = zeros(numNodes, numTimes, numRuns);
+    storedStates = zeros(Int64, numNodes, numTimes);
 
-for kRuns = 1:numRuns
+    storedS = zeros(Int64, numNodes, numTimes);
+    storedI = zeros(Int64, numNodes, numTimes);
+    storedR = zeros(Int64, numNodes, numTimes);
 
-    currTime = 0;
-    currState = copy(initConds);
-    tPosition = 1;
-    storedStates[:, tPosition, kRuns] = copy(currState);
+    @time for kRuns = 1:numRuns
 
-    infNodes = vec(initConds.==1);
-    numInfNodes = Int(sum(infNodes)); 
-    infNodesList = zeros(Int, numNodes, 1);
-    infNodesList[1:numInfNodes] = findall(infNodes);
+        currTime = 0.0;
+        currState = copy(initConds);
+        tPosition = 1;
+        storedStates[:, tPosition] = currState;
 
-    possLinks = copy(Adj);
-    possLinks[:, infNodesList[1:numInfNodes]].=0;
-    possLinks = dropzeros(possLinks);
+        infNodes = vec(initConds.==1);
+        numInfNodes = sum(infNodes);
+        infNodesList = zeros(Int, numNodes, 1);
+        infNodesList[1:numInfNodes] = findall(infNodes);
 
-    vulNodes = possLinks[infNodesList[1:numInfNodes], :];
-    vulNodesPairs = Tuple.(findall(x->x==1, vulNodes));
+        possLinks = copy(Adj);
+        possLinks[:, infNodesList[1:numInfNodes]].=0;
+        possLinks = dropzeros(possLinks);
 
-    vulNodesList = [];
+        vulNodes = possLinks[infNodesList[1:numInfNodes], :];
+        vulNodesPairs = Tuple.(findall(x->x==1, vulNodes));
 
-    for i = 1:length(vulNodesPairs)
-        push!(vulNodesList, vulNodesPairs[i][2]);
-    end
+        while currTime < maxTime
 
-    while currTime < maxTime
+            oldState = copy(currState);
 
-        oldState = copy(currState);
+            infRate = lambda*size(vulNodesPairs)[1];
+            recRate = gamma*sum(infNodes);
+            eventRate = infRate + recRate;
 
-        infRate = lambda*length(vulNodesList);
-        recRate = gamma*sum(infNodes);
-        eventRate = infRate + recRate;
+            timeStep = rand(Exponential(1/eventRate));
 
-        timeStep = rand(Exponential(1/eventRate));
+            randEventProb = rand();
 
-        randEventProb = rand();
+            if randEventProb < infRate/eventRate
 
-        if randEventProb < infRate/eventRate
+                newlyInfNode = rand(vulNodesPairs)[2];
+                currState[newlyInfNode] = 1;
+                infNodes[newlyInfNode] = true;
 
-            newlyInfNode = rand(vulNodesList);
-            currState[newlyInfNode] = 1;
-            infNodes[newlyInfNode] = true;
-            numInfNodes += 1;
-            infNodesList[numInfNodes] = newlyInfNode;
+                numInfNodes += 1;
+                infNodesList[numInfNodes] = newlyInfNode;
 
-            possLinks[:, newlyInfNode] .= 0;
-            possLinks = dropzeros(possLinks);
-                
-            vulNodes = possLinks[infNodesList[1:numInfNodes], :];
-            vulNodesPairs = Tuple.(findall(x->x==1, vulNodes));
+                possLinks[:, newlyInfNode] .= 0;
+                possLinks = dropzeros(possLinks);
+            
+            else
 
-            vulNodesList = [];
+                chosenRecNodeIndex = rand(1:numInfNodes);
+                newlyRecNode = infNodesList[chosenRecNodeIndex];
 
-            for i = 1:length(vulNodesPairs)
-                push!(vulNodesList, vulNodesPairs[i][2]);
+                infNodesList[chosenRecNodeIndex] = infNodesList[numInfNodes];
+                infNodesList[numInfNodes] = 0;
+
+                currState[newlyRecNode] = 2;
+                infNodes[newlyRecNode] = false;
+                numInfNodes -= 1;
+
+                possLinks[newlyRecNode, :] .= 0;
+                possLinks = dropzeros(possLinks);
+            
             end
-        
-        else
-
-            chosenRecNodeIndex = rand(1:numInfNodes);
-            newlyRecNode = infNodesList[chosenRecNodeIndex];
-
-            infNodesList[chosenRecNodeIndex] = infNodesList[numInfNodes];
-            infNodesList[numInfNodes] = 0;
-
-            currState[newlyRecNode] = 2;
-            infNodes[newlyRecNode] = false;
-            numInfNodes -= 1;
-
-            possLinks[newlyRecNode, :] .= 0;
-            possLinks = dropzeros(possLinks);
 
             vulNodes = possLinks[infNodesList[1:numInfNodes], :];
             vulNodesPairs = Tuple.(findall(x->x==1, vulNodes));
 
-            vulNodesList = [];
+            currTime += timeStep;
 
-            for i = 1:length(vulNodesPairs)
-                push!(vulNodesList, vulNodesPairs[i][2]);
+            if currTime > maxTime
+                currTime = maxTime;
+                tPositionNew = numTimes;
+            else
+                tPositionNew = floor(Int64, currTime/timeRes);
             end
+
+            if tPositionNew > tPosition
+                oldStateRepeated = oldState*ones(Int64, 1, tPositionNew - tPosition);
+                storedStates[:, (tPosition+1):tPositionNew] = oldStateRepeated;
+                tPosition = tPositionNew;
+            end
+
+            if sum(infNodes) < 1
+                currTime = maxTime;
+            end
+
+        end
+
+        if tPosition < numTimes
+            endStateRepeated = currState*ones(Int64, 1, numTimes - tPosition);
+            storedStates[:, (tPosition+1):numTimes] = endStateRepeated;
+        end
+
+        if kRuns == 1
+
+            storedS = storedStates.==0;
+            storedI = storedStates.==1;
+            storedR = storedStates.==2;
+        
+        else
+
+            storedS += storedStates.==0;
+            storedI += storedStates.==1;
+            storedR += storedStates.==2;
         
         end
 
-        currTime += timeStep;
-
-        if currTime > maxTime
-            currTime = maxTime;
-            tPositionNew = numTimes;
-        else
-            tPositionNew = Int(floor(currTime/timeRes));
-        end
-
-        if tPositionNew > tPosition
-            oldStateRepeated = oldState*ones(1, tPositionNew - tPosition);
-            storedStates[:, (tPosition+1):tPositionNew, kRuns] = copy(oldStateRepeated);
-            tPosition = tPositionNew;
-        end
-
-        if sum(infNodes) < 1
-            currTime = maxTime;
-        end
-
     end
 
-    if tPosition < numTimes
-        endStateRepeated = currState*ones(1, numTimes - tPosition);
-        storedStates[:, (tPosition+1):numTimes, kRuns] = copy(endStateRepeated);
-    end
+    probS = storedS/numRuns;
+    probI = storedI/numRuns;
+    probR = storedR/numRuns;
+
+    return probS, probI, probR
 
 end
 
-storedStates
+# @profview runSIRGillespie()
+probS, probI, probR = runSIRGillespie();
 
-probS = sum(storedStates .== 0, dims=3)/numRuns
-probI = sum(storedStates .== 1, dims=3)/numRuns
-probR = sum(storedStates .== 2, dims=3)/numRuns
+probS
+probI
+probR
