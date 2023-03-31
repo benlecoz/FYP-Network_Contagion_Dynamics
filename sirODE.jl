29e7eb7c0e0a5bbec9156cc9d1a38c09ad37ca5f
benlecoz <go19111@bristol.ac.uk>
2023-03-31 16:08:57 +0100

    working indiv ODE tracking

--- sirODE.jl
+++ sirODE.jl
@@ -1,6 +1,8 @@
 include("./generateAdj.jl")
 using .AdjGenerator, Graphs, SparseArrays
 using DifferentialEquations, BenchmarkTools
+using Plots
+using InvertedIndices
 
 numNodes = 100;
 
@@ -11,14 +13,8 @@ lambda = 1;
 gamma = 0.1;
 modelParams = [lambda, gamma];
 
-maxTime = 12.0;
+maxTime = 50.0;
 timeRes = 0.01;
-
-initConds = zeros(Int64, numNodes, 2);
-initConds[1, 2] = 1;
-initConds[2:end, 1] .= 1;
-
-numRuns = 10^3;
 
 # function sirODE(numNodes::Int64, graphType::String, graphParams::Array, modelParams::Array, maxTime::Float64, 
 #     timeRes::Float64, initConds::Array)
@@ -26,38 +22,56 @@ numRuns = 10^3;
 graph = AdjGenerator.generateAdj(numNodes, graphType, graphParams);
 edgeArray = Tuple.(edges(graph));
 
+initInfNodes = [1, 3];
+
+initConds = zeros(Int64, nv(graph), 2);
+initConds[initInfNodes, 2] .= 1;
+initConds[Not(initInfNodes), 1] .= 1;
+
 maxTime = timeRes*ceil(maxTime/timeRes);
 t = Array(collect(Float64, 0:timeRes:maxTime)');
 
+function edgeArrayGenList(nodeList, side)
+    if side == "both"
+        edgesNodeList = unique([reduce(vcat, [findall(x->x==i, first.(edgeArray)) for i in nodeList])..., (reduce(vcat, [findall(x->x==i, last.(edgeArray)) for i in nodeList])...)])
+    elseif side == "LHS"
+        edgesNodeList = reduce(vcat, [findall(x->x==i, first.(edgeArray)) for i in nodeList])
+    elseif side == "RHS"
+        edgesNodeList = reduce(vcat, [findall(x->x==i, last.(edgeArray)) for i in nodeList])
+    end
+
+    return edgesNodeList
+end;
+
+u0 = [initConds[:, 1]; initConds[:, 2]];
+
+
 function diffEq(du, u, p, t)
 
-    modelRates = zeros(numNodes, numNodes);
-    s = u[:, 1];
-    i = u[:, 2];
+    s = u[1 : p[1]];
+    i = u[(p[1] + 1) : 2*p[1]];
 
-    adjIndex = (first.(edgeArray) .- 1) * numNodes + last.(edgeArray);
+    adjIndex = (first.(edgeArray) .- 1) * p[1] + last.(edgeArray);
 
-    modelRates[adjIndex] = lambda .* s[first.(edgeArray)] .* i[last.(edgeArray)];
+    infRates = zeros(p[1], p[1]);
+    infRates[adjIndex] = lambda .* s[last.(edgeArray)] .* i[first.(edgeArray)];
+    totalInfRate = sum(infRates, dims = 2);
 
-    totalInfRate = sum(modelRates);
-
-    for k=1:numNodes
-        du[k, 1] = -totalInfRate;
-        du[k, 2] = totalInfRate - gamma*i[k]
-    end
+    du[1 : p[1]] .= -totalInfRate;
+    du[(p[1] + 1) : 2*p[1]] .= totalInfRate .- gamma*i
 
 end;
 
-prob = ODEProblem(diffEq, initConds, (0, maxTime));
-sol = solve(prob)
+p = [nv(graph), numEdges];
 
-# sSol = sol[1:numNodes,:]
-# iSol = sol[(numNodes+1):2*numNodes,:]
-# rSol = 1 .- sSol .- iSol
+prob = ODEProblem(diffEq, u0, (0, maxTime), p, dt = 0.01, saveat=t);
+sol = solve(prob);
 
-sSol = sol[:, 1, :]
-iSol = sol[:, 2, :]
-rSol = 1 .- sSol .- iSol
+sSol = sol[1:p[1], :];
+iSol = sol[(p[1] + 1):2*p[1], :];
+rSol = 1 .- sSol .- iSol;
 
+plot(sSol', label="")
 plot(iSol', label="")
+plot(rSol', label="")
 
