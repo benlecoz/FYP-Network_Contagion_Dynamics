39f986bb01963cfd40a22ddb25c18f03d0547573
benlecoz <go19111@bristol.ac.uk>
2023-04-13 15:47:25 +0100

    Fixed inaccuracies in the code

--- sirPairODE.jl
+++ sirPairODE.jl
@@ -1,142 +1,113 @@
+module sirPairODE
+
 include("./generateAdj.jl")
-using .AdjGenerator, Graphs, SparseArrays
-using DifferentialEquations, BenchmarkTools
-using Plots
-using InvertedIndices
+using .AdjGenerator: edgeArrayGenList 
+using Graphs, SparseArrays, DifferentialEquations
+using Plots, InvertedIndices, BenchmarkTools
 
-# number of nodes
-numNodes = 100;
+function SIRPairODE(edgeArray::Array, graph::SimpleGraph, modelParams::Array, maxTime::Float64, timeRes::Float64, initInfNodes::Array)
 
-# graph type and associated parameters
-graphType = "WattsStrogatz";
-graphParams = [4, 0.5];
+    lambda, gamma = modelParams;
+    numNodes = nv(graph);
+    
+    # edgeArray only has directed edges, so create new array of edges with oppositely directed edges for every node pair
+    edgeArrayNew = copy(edgeArray);
+    for (i, t) in pairs(edgeArray)
+        push!(edgeArrayNew, reverse(t))
+    end;
+    edgeArrayNew = sort(edgeArrayNew);
 
-# model parameters
-lambda = 1;
-gamma = 0.1;
-modelParams = [lambda, gamma];
+    # set the initial conditions of each nodes
+    initConds = zeros(Int64, numNodes, 2);
+    initConds[initInfNodes, 2] .= 1;
+    initConds[Not(initInfNodes), 1] .= 1;
 
-# time parameters
-maxTime = 50.0;
-timeRes = 0.01;
+    # extract time components
+    maxTime = timeRes*ceil(maxTime/timeRes);
+    t = Array(collect(Float64, 0:timeRes:maxTime)');
 
-# generate and its array of edges
-graph = AdjGenerator.generateAdj(numNodes, graphType, graphParams);
-edgeArray = Tuple.(edges(graph));
+    # calculate number of directed edges 
+    numEdges = length(edgeArrayNew);
 
-# edgeArray only has directed edges, so generate oppositely directed edges for every node pair
-for (i, t) in pairs(edgeArray)
-    push!(edgeArray, reverse(t))
-end;
-edgeArray = sort(edgeArray);
-
-# set initially infected node(s)
-initInfNodes = [1, 3];
-
-# set the initial conditions of each nodes
-initConds = zeros(Int64, nv(graph), 2);
-initConds[initInfNodes, 2] .= 1;
-initConds[Not(initInfNodes), 1] .= 1;
-
-# extract time components
-maxTime = timeRes*ceil(maxTime/timeRes);
-t = Array(collect(Float64, 0:timeRes:maxTime)');
-
-# generate list of neighbour vertices for each node, based on which side of the edgeArray node pair it is on 
-function edgeArrayGenList(nodeList, side)
-    if side == "both"
-        edgesNodeList = unique([reduce(vcat, [findall(x->x==i, first.(edgeArray)) for i in nodeList])..., (reduce(vcat, [findall(x->x==i, last.(edgeArray)) for i in nodeList])...)])
-    elseif side == "LHS"
-        edgesNodeList = reduce(vcat, [findall(x->x==i, first.(edgeArray)) for i in nodeList])
-    elseif side == "RHS"
-        edgesNodeList = reduce(vcat, [findall(x->x==i, last.(edgeArray)) for i in nodeList])
+    # ensure that edges containing two initially infected nodes are not accidentally marked as S-I 
+    initIIEdges = zeros(Int64, 0);
+    for i in AdjGenerator.edgeArrayGenList(edgeArrayNew, initInfNodes, "RHS")
+        if in.(first.(edgeArrayNew)[i], Ref(initInfNodes))
+            append!(initIIEdges, i)
+        end
     end
+    
+    # filter the I-I edges from the S-I and I-S edges
+    initSIEdges = filter!(x->!(x in initIIEdges), AdjGenerator.edgeArrayGenList(edgeArrayNew, initInfNodes, "RHS"));
 
-    return edgesNodeList
-end;
+    # based on initially infected nodes and edgeArray, set the initial pair conditions of the S-S and S-I states
+    initPairConds = zeros(Int64, numEdges, 2);
+    initPairConds[initSIEdges, 2] .= 1;
+    initPairConds[Not(edgeArrayGenList(edgeArrayNew, initInfNodes, "both")), 1] .= 1;
 
-# calculate number of directed edges 
-numEdges = length(edgeArray);
+    # set the parameters associated with the ODEProblem
+    p = [lambda, gamma];
 
-# based on initially infected nodes and edgeArray, set the initial pair conditions of the S-S and S-I states
-initPairConds = zeros(Int64, numEdges, 2);
-initPairConds[edgeArrayGenList(initInfNodes, "RHS"), 2] .= 1;
-initPairConds[Not(edgeArrayGenList(initInfNodes, "both")), 1] .= 1;
+    # for each node, find the edge pairs for which the node is on the LHS (will be used to find which nodes are in the S-I state)  
+    edgeArrayIndexLHS = Array[];
+    for j=1:numNodes
+        append!(edgeArrayIndexLHS, [edgeArrayGenList(edgeArrayNew, j, "LHS")])
+    end;
 
-# set the parameters associated with the ODEProblem
-p = [nv(graph), numEdges];
+    # set the initial guess
+    u0 = [initConds[:, 1]; initConds[:, 2]; initPairConds[:, 1]; initPairConds[:, 2]];
 
-# for each node, find the edge pairs for which the node is on the LHS (will be used to find which nodes are in the S-I state)  
-edgeArrayIndexLHS = Array[];
-for j=1:p[1]
-    append!(edgeArrayIndexLHS, [edgeArrayGenList(j, "LHS")])
-end;
+    # differential equation generator
+    function diffEq(du, u, p, t)
 
-# set the initial guess
-u0 = [initConds[:, 1]; initConds[:, 2]; initPairConds[:, 1]; initPairConds[:, 2]];
+        lambda, gamma = p
 
-# differential equation generator
-function diffEq(du, u, p, t)
+        # extract each of the states from the initial guess
+        s = u[1 : numNodes];
+        i = u[(numNodes + 1) : 2*numNodes];
+        ss = u[(2*numNodes + 1) : (2*numNodes + numEdges)];
+        si = u[(2*numNodes + numEdges + 1) : end];
 
-    # extract each of the states from the initial guess
-    s = u[1 : p[1]];
-    i = u[(p[1] + 1) : 2*p[1]];
-    ss = u[(2*p[1] + 1) : (2*p[1] + p[2])];
-    si = u[(2*p[1] + p[2] + 1) : end];
+        # for each node, calculate the number of times the node is on the LHS of a S-I pairing
+        sumSI = zeros(numNodes, 1);
+        for k=1:numNodes
+            sumSI[k] = lambda*sum(si[edgeArrayIndexLHS[k]])
+        end
 
-    # for each node, calculate the number of times the node is on the LHS of a S-I pairing
-    sumSI = zeros(p[1], 1);
-    for k=1:p[1]
-        sumSI[k] = sum(si[edgeArrayIndexLHS[k]])
-    end
+        # calculate the sums of S-I states, depending on whether the susceptible node is the i or j node in the SiIj expression
+        neighbourInfRHS = (sumSI[last.(edgeArrayNew)] - si) ./ s[last.(edgeArrayNew)];
+        neighbourInfLHS = (sumSI[first.(edgeArrayNew)] - si) ./ s[first.(edgeArrayNew)]; 
 
-    # calculate the sums of S-I states, depending on whether the S node is the i or j node in the SiIj expression
-    neighbourInfRHS = (sumSI[last.(edgeArray)] - si) ./ s[last.(edgeArray)];
-    neighbourInfLHS = (sumSI[first.(edgeArray)] - si) ./ s[first.(edgeArray)]; 
+        # test if the S value in the above sums are non-zero by testing whether the result is finite
+        for i=1:numEdges
+            neighbourInfRHS[i] = isfinite(neighbourInfRHS[i]) ? neighbourInfRHS[i] : 0
+            neighbourInfLHS[i] = isfinite(neighbourInfLHS[i]) ? neighbourInfLHS[i] : 0
+        end
 
-    # test if the S value in the above sums are different than 0 by testing whether the result is finite
-    for i=1:p[2]
-        neighbourInfRHS[i] = isfinite(neighbourInfRHS[i]) ? neighbourInfRHS[i] : 0
-        neighbourInfLHS[i] = isfinite(neighbourInfLHS[i]) ? neighbourInfLHS[i] : 0
-    end
+        # set the differential equations associated with the S, I, S-S and S-I states
+        du[1 : numNodes] .= -sumSI;
+        du[(numNodes + 1) : 2*numNodes] .= sumSI .- gamma*i
 
-    # set the differential equations associated with the S, I, S-S and S-I states
-    du[1 : p[1]] .= -lambda * sumSI;
-    du[(p[1] + 1) : 2*p[1]] .= lambda * sumSI .- gamma*i
+        du[(2*numNodes + 1) : (2*numNodes + numEdges)] = - ss.*neighbourInfLHS - ss.*neighbourInfRHS
+        du[(2*numNodes + numEdges + 1) : end] = ss.*neighbourInfRHS - si.*neighbourInfLHS - lambda*si - gamma*si
 
-    du[(2*p[1] + 1) : (2*p[1] + p[2])] = - ss.*neighbourInfLHS - ss.*neighbourInfRHS
-    du[(2*p[1] + p[2] + 1) : end] = ss.*neighbourInfRHS - si.*neighbourInfLHS - lambda*si - gamma*si
+    end;
 
-end;
+    # set the ODE problem and solve it
+    prob = ODEProblem(diffEq, u0, (0, maxTime), p, dt = timeRes, saveat=t);
+    sol = solve(prob);
 
-# set the ODE problem and solve it
-prob = ODEProblem(diffEq, u0, (0, maxTime), p, dt = 0.01, saveat=t);
-sol = solve(prob);
+    # extract the solutions of the individual states
+    sSol = sol[1:numNodes, :]
+    iSol = sol[(numNodes + 1):2*numNodes, :]
+    rSol = 1 .- sSol .- iSol
 
-# extract and plot the solutions of the individual states
-sSol = sol[1:p[1], :]
-iSol = sol[(p[1] + 1):2*p[1], :]
-rSol = 1 .- sSol .- iSol
+    # extract the solutions of the pair states
+    ssSol = sol[(2*numNodes + 1) : (2*numNodes + numEdges), :]
+    siSol = sol[(2*numNodes + numEdges + 1) : end, :]
 
-plot(sSol', label="")
-plot(iSol', label="")
-plot(rSol', label="")
+    return sSol, iSol, rSol, ssSol, siSol
 
-# extract and plot the solutions of the pair states
-ssSol = sol[(2*p[1] + 1) : (2*p[1] + p[2]), :]
-siSol = sol[(2*p[1] + p[2] + 1) : end, :]
+end
 
-plot(ssSol', label="")
-plot(siSol', label="")
-
-
-
-# s = u0[1 : p[1]]
-# i = u0[(p[1] + 1) : 2*p[1]]
-# ss = u0[(2*p[1] + 1) : (2*p[1] + p[2])]
-# si = u0[(2*p[1] + p[2] + 1) : end]
-
-# neighbourInfRHS = ss.*(sumSI[last.(edgeArray)] - si) ./ s[last.(edgeArray)]
-# neighbourInfLHS = (sumSI[first.(edgeArray)] - si) ./ s[first.(edgeArray)]
-
-
+end
